61bc366cdf785b50e7988b8b7ac1402784d5d896
Peter Rayner <peter.rayner3@gmail.com>
2019-07-18 09:46:43 +0200

    added changed to better handle end of month.

--- athena_time_partition.sh
+++ athena_time_partition.sh
@@ -9,9 +9,13 @@
  previousday=$(date -d "-1 day" +"%Y/%m/%d") #gets previous day in 2019/07/31 format
  DATABASE="testdb" #name of my Athena database
  TABLENAME="testtable" #name of my Athena table in said database
- MainQuery="Select * from $DATABASE.$TABLENAME where year=$(date +"%Y") and month=$(date +"%m") and day>=$(date -d "-1 day" +"%d")"  #use this for the main query at the end which will query all the data from yesterdays partition that will be added
- queryString="ALTER TABLE  $DATABASE.$TABLENAME ADD PARTITION (dt = '$previousday') ;" #use this query to add the partitions to the base table before the main query happens
+ previousdayquery=$(date -d "-1 day" +"year = '%Y' and month = '%m'and day >= '%d'")
+ MainQuery="Select name,surname from $DATABASE.$TABLENAME where $previousdayquery"  #use this for the main query at the end which will query all the data from yesterdays partition that will be added
+ previousdaypartition=$(date -d "-1 day" +"year = '%Y', month = '%m', day = '%d'")
 
+ queryString="ALTER TABLE  $DATABASE.$TABLENAME ADD PARTITION ($previousdaypartition) ;" #use this query to add the partitions to the base table before the main query happens
+echo $queryString
+echo $MainQuery
 #query for adding the partitions for the previous day. 
 # if this is the first time you are running this query you may want to run the following command first to load all previous days partitions, warning if you have 10s of thousands of partitions this could cause issues, you may have to go through each one manually with a for loop.
 #
@@ -24,10 +28,10 @@
 #
 # THIS RUNS THE ADD PARTITION command. THE OUTPUT LOCATION IS THE S3 LOCATION WHERE YOU WILL FIND THE RESULTS OF YOUR DATA.
 #  RESULT RETURNS THE QUERY id OF THE query
-result=$(aws athena start-query-execution --query-string "$queryString" --result-configuration "OutputLocation=s3://<BUCKETNAME>/outputresults/")
+result=$(aws athena start-query-execution --query-string "$queryString" --result-configuration "OutputLocation=s3://soaringsafaris/outputresults/")
 echo $result #output the query ID for queryString
 
-# halts the script for 5s to ensure the partition is added and the query completes succesfully, before running main script
+# halts the script for 5s to ensure the partition is added and the query completes succesfully, before running main script. You can remove this is you do not have any resource contention
 sleep 5
 
 
@@ -36,6 +40,6 @@ then
 	#
 	# THIS RUNS THE MAIN QUERY. THE OUTPUT LOCATION IS THE S3 LOCATION WHERE YOU WILL FIND THE RESULTS OF YOUR DATA.
 	#
-	result=$(aws athena start-query-execution --query-string "$MainQuery" --result-configuration "OutputLocation=s3://<BUCKETNAME>/outputresults/")
+	result=$(aws athena start-query-execution --query-string "$MainQuery" --result-configuration "OutputLocation=s3://soaringsafaris/outputresults/")
 	echo $result #output the query ID for MainQuery
 fi
