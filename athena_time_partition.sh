4d00ef29283bf0a64d10d7ee9c87e5f927b44e32
Peter Rayner <peter.rayner3@gmail.com>
2019-07-19 14:57:11 +0200

    update changes for region
    region added for use on datapipeline

--- athena_time_partition.sh
+++ athena_time_partition.sh
@@ -1,9 +1,6 @@
  #! /bin/bash
 
- # !!!!!!!!!!-------------------THIS SCRIPT IS USED AT YOUR OWN RISK----------------------------!!!!!!!!!
- # !!!!!!!!!!I HAVE NOT CHECKED FOR ALL POSSIBLE EDGE CASES AND IT IS POSSIBLE THERE ARE BUGS IN HERE!!!!
- #
- #
+ # THIS SCRIPT IS USED AT YOUR OWN RISK
  #THIS SCRIPT ASSUMES YOU HAVE AWS CLI installed, if not use sudo apt-get install awscli -- or see : https://docs.aws.amazon.com/cli/latest/userguide/cli-chap-install.html
  #THIS SCRIPT IS MADE TO ADD THE LAST DAY PARTITION AND THEN QUERY ALL THE DATA FROM THIS LAST DAY
  #
@@ -23,7 +20,7 @@ echo $MainQuery
 # if this is the first time you are running this query you may want to run the following command first to load all previous days partitions, warning if you have 10s of thousands of partitions this could cause issues, you may have to go through each one manually with a for loop.
 #
 #
-# result=$(aws athena start-query-execution --query-string "msck repair table $DATABASE.$TABLENAME" --result-configuration "OutputLocation=s3://soaringsafaris/outputresults/")
+# result=$(aws athena start-query-execution --query-string "msck repair table $DATABASE.$TABLENAME" --result-configuration "OutputLocation=s3://<BUCKETNAME>/outputresults/")
 # echo $result
 #
 #
@@ -31,7 +28,7 @@ echo $MainQuery
 #
 # THIS RUNS THE ADD PARTITION command. THE OUTPUT LOCATION IS THE S3 LOCATION WHERE YOU WILL FIND THE RESULTS OF YOUR DATA.
 #  RESULT RETURNS THE QUERY id OF THE query
-result=$(aws athena start-query-execution --query-string "$queryString" --result-configuration "OutputLocation=s3://soaringsafaris/outputresults/")
+result=$(aws athena start-query-execution --query-string "$queryString" --result-configuration "OutputLocation=s3://<BUCKETNAME>/outputresults/" --region eu-west-1)
 echo $result #output the query ID for queryString
 
 # halts the script for 5s to ensure the partition is added and the query completes succesfully, before running main script. You can remove this is you do not have any resource contention
@@ -43,6 +40,18 @@ then
 	#
 	# THIS RUNS THE MAIN QUERY. THE OUTPUT LOCATION IS THE S3 LOCATION WHERE YOU WILL FIND THE RESULTS OF YOUR DATA.
 	#
-	result=$(aws athena start-query-execution --query-string "$MainQuery" --result-configuration "OutputLocation=s3://soaringsafaris/outputresults/")
+	result=$(aws athena start-query-execution --query-string "$MainQuery" --result-configuration "OutputLocation=s3://<BUCKETNAME>/outputresults/" --region eu-west-1)
 	echo $result #output the query ID for MainQuery
 fi
+#
+#
+# (sudo yum -y update aws-cli) && (aws s3 sync s3://<BUCKETNAME>/bashscript/ /home/ec2-user/) && (chmod 777 /home/ec2-user/athena_time_partition.sh) && (/home/ec2-user/athena_time_partition.sh) 
+#
+#
+#
+#
+#
+#
+#
+#
+#
